e55290bd19ccc9cc09d3fa9b6a9f58286df4a650
zen510 <iszako6814@gmail.com>
2019-12-04 22:20:26 +0900

    mode1:種別のみ対応

--- src/server.py
+++ src/server.py
@@ -15,15 +15,32 @@ options.hardware_mapping = 'adafruit-hat'
 
 matrix = RGBMatrix(options = options)
 
+flag = False
+blightness = 1
+
+
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def post_json():
-    json = request.get_json()
-  print(json["mode"])
+  json = request.get_json()
+  print(str(json["mode"]))
   print(json["type"])
   print(json["dest"])
   print(json["dest2"])
+  return json
   
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=61101)
+
+
+def set_image(Mode,Type,Dest,Dest2):
+
+  blightness = 1
+
+  imageType = Image.open("../img/type/" + Mode +".png" )
+
+  if(mode == 1):
+    imageType.point(lambda x: x * blightness)
+    imageType.convert('RGB')
+    matrix.SetImage(imageType)
